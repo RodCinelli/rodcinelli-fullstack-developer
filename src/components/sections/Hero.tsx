e074cc422bcd7d5841e25e22a327dc2b4b9f2151
Rodrigo Cinelli <rodcinelli@gmail.com>
2025-02-26 12:04:44 -0300

    refactor: Adjust Hero section mobile top padding

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -22,12 +22,12 @@ const HeroSection = styled.section`
   @media (max-width: 768px) {
     padding: 0 1.5rem;
     justify-content: flex-start;
-    padding-top: 6rem;
+    padding-top: 7rem;
   }
   
   @media (max-width: 520px) {
     padding: 0 1rem;
-    padding-top: 5rem;
+    padding-top: 6rem;
   }
 `
 
