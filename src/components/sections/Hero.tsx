5f9d30fb2bcb4515abff0d298134c46c66099205
Rodrigo Cinelli <rodcinelli@gmail.com>
2025-02-26 12:06:20 -0300

    refactor: Fine-tune Hero section mobile top padding

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -22,12 +22,12 @@ const HeroSection = styled.section`
   @media (max-width: 768px) {
     padding: 0 1.5rem;
     justify-content: flex-start;
-    padding-top: 7rem;
+    padding-top: 6.5rem;
   }
   
   @media (max-width: 520px) {
     padding: 0 1rem;
-    padding-top: 6rem;
+    padding-top: 5.5rem;
   }
 `
 
